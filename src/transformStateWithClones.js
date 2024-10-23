d912b8f095b2fa8b1fd11aa78c211bb98092d020
Yevhen Loboda <yevhen.loboda1986@gmail.com>
2024-10-23 21:17:37 +0200

    Done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,32 @@
  *
  * @return {Object[]}
  */
+
 function transformStateWithClones(state, actions) {
   // write code here
+  let clone = { ...state };
+  const story = [];
+
+  for (const action of actions) {
+    clone = { ...clone };
+
+    if (action.type === 'addProperties') {
+      Object.assign(clone, action.extraData);
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete clone[key];
+      }
+    }
+
+    if (action.type === 'clear') {
+      clone = {};
+    }
+    story.push(clone);
+  }
+
+  return story;
 }
 
 module.exports = transformStateWithClones;
